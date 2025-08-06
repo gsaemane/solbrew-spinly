ecb87f97d4e7812286725f3e39bbb75e2d501489
Geolynx Saemane <gsaemane@genisys.com.sb>
2025-08-07 09:10:54 +1100

    Fox stock manager

--- solbrew-spinly/src/components/stockManager.tsx
+++ solbrew-spinly/src/components/stockManager.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 const StockManager: React.FC = () => {
   const [items, setItems] = useState<StockItem[]>([]);
-  const [newItem, setNewItem] = useState({id:'', name: '', quantity: 0, color: '#D5AE60', image: '', isWinner:true });
+  const [newItem, setNewItem] = useState<StockItem>({ id: '', name: '', image: '', color: '#000000', isWinner: true, quantity: 0 });
   const [editingId, setEditingId] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
 
@@ -194,8 +194,9 @@ const StockManager: React.FC = () => {
 
         </div>
         
-        <button onClick={handleSaveItem} className=" text-red-200 p-3 w-1/2 m-auto mt-3 rounded hover:bg-spot-gold hover:text-spot-black lg:bg-red-600">
-          Add Item
+        <button onClick={handleSaveItem} className=" text-red-200 p-3 w-1/2 m-auto mt-3 rounded hover:bg-spot-gold hover:text-red-300 hover:bg-red-700 hover:cursor-pointer lg:bg-red-600">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className='inline mr-1' fill="#d5ae60" viewBox="0 0 256 256"><path d="M208,40H176V80a8,8,0,0,1-8,8H88a8,8,0,0,1-8-8V40.73a8,8,0,0,0-2.34,1.61L42.34,77.66A8,8,0,0,0,40,83.31V208a8,8,0,0,0,8,8H208a8,8,0,0,0,8-8V48A8,8,0,0,0,208,40ZM128,184a32,32,0,1,1,32-32A32,32,0,0,1,128,184Z" opacity="0.2"></path><path d="M208,32H83.31A15.86,15.86,0,0,0,72,36.69L36.69,72A15.86,15.86,0,0,0,32,83.31V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM88,48h80V80H88ZM208,208H48V83.31l24-24V80A16,16,0,0,0,88,96h80a16,16,0,0,0,16-16V48h24Zm-80-96a40,40,0,1,0,40,40A40,40,0,0,0,128,112Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,176Z"></path></svg>
+          Save Item
         </button>
       </div>
       {/* item list */}
